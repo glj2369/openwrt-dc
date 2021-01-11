ed888fadeb0705157f382b0c1f77d31c22474ee1
glj2369 <glj2369@126.com>
2021-01-11 13:33:28 +0800

    up

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,6 +10,9 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 git clone https://github.com/Zxilly/UA2F.git
+git clone https://github.com/glj2369/ruijie.git
+git clone https://github.com/kenzok8/openwrt-packages.git
+git clone https://github.com/kenzok8/small.git
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
